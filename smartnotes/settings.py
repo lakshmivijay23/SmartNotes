f00b25e5c0384c3e8a6df2d44d6be251c6cbb512
Lakshmi Vijayan <146443780+lakshmivijay23@users.noreply.github.com>
2025-12-13 11:44:10 +0000

    added dynamic template

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = "smartnotes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'static/templates',
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS=[
+    BASE_DIR / "static",
+]
+
