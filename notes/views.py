74851216f769d9607eea7e0966894942dc38a23a
Lakshmi Vijayan <146443780+lakshmivijay23@users.noreply.github.com>
2025-12-14 15:10:38 +0000

    Added validation form checking

--- notes/views.py
+++ notes/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
 from .forms import NotesForm
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def add_like_view(request, pk):
     if request.method=="POST":
@@ -32,11 +33,20 @@ class NotesCreateView(CreateView):
     success_url='/smart/notes'
     form_class=NotesForm
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object=form.save(commit=False)
+        self.object.user=self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name="notes"
     template_name='notes/notes_list.html'
+    login_url="/admin"
 
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model=Notes
