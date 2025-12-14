bb5372c289f60967a40ddfb614e251cbcaea7297
Lakshmi Vijayan <146443780+lakshmivijay23@users.noreply.github.com>
2025-12-14 09:22:14 +0000

    Working with existing data: compeleted

--- notes/views.py
+++ notes/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse_lazy
 from .models import Notes
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
 from .forms import NotesForm
+from django.urls import reverse
+
+def add_like_view(request, pk):
+    if request.method=="POST":
+        note = get_object_or_404(Notes, pk=pk)
+        note.likes+=1
+        note.save()
+        return HttpResponseRedirect(reverse("notes.detail", args=(pk,)))
+    raise Http404
 
 class NotesUpdateView(UpdateView):
     model=Notes
     success_url='/smart/notes'
     form_class=NotesForm
 
-# class NotesDeleteView(DeleteView):
-#     model=Notes
-#     success_url='/smart/notes'
+class NotesDeleteView(DeleteView):
+    model=Notes
+    # success_url='/smart/notes'
+    success_url = reverse_lazy('notes.list')
+    template_name='notes/notes_delete.html'
     
 class NotesCreateView(CreateView):
     model=Notes
